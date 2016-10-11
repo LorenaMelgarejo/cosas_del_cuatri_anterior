f36520b4b97bd56c9a97800b5e4104895d329be8
santiagolo90 <santiago.lo90@gmail.com>
2016-10-11 14:02:09 -0300

    TP 2- Laboratorio 2

---
+++ Alumno_Clase_12_LM/Clase_12_Library/Automovil.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_12_Library
+{
+    public class Automovil:Vehiculo // falto nombrar a la clase padre
+    {
+        public Automovil(EMarca marca, string patente, ConsoleColor color)
+            : base(marca,patente, color)
+        {
+        }
+        /// <summary>
+        /// Los automoviles tienen 4 ruedas
+        /// </summary>
+        public override short CantidadRuedas//se agrega set y public
+        {
+            get
+            {
+                return 4;
+            }
+            set { }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("AUTOMOVIL");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("RUEDAS : "+ this.CantidadRuedas);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
